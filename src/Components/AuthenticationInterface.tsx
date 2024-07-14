9519914b115e8a1703a51382edeef5c3f3287b57
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-07-15 00:16:00 +0200

    hella nais registration validation

--- src/Components/AuthenticationInterface.tsx
+++ src/Components/AuthenticationInterface.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, SetStateAction } from 'react';
 import { Button, TextField, Typography, Divider } from '@mui/material';
 import { signInWithGoogle, signInWithEmailAndPassword, registerUser, onAuthStateChange } from '../Utils/Firebase'; // Adjust the import paths as necessary
 import { Google } from '@mui/icons-material';
@@ -18,74 +18,231 @@ const Wrapper = styled.div`
     flex-direction: column;
     gap: 10px;
 `;
+
+const signInRequirements = {
+    "isInUse": "E-postadressen er allerede i bruk",
+    "incorrectPassword": "Feil passord eller e-postadresse",
+    "noUser": "Brukeren finnes ikke",
+};
+
+const emailRequirements = {
+    "correctFormat": "E-postadressen må være i riktig format",
+};
+
+const passwordRequirements = {
+    "sixCharacters": "Passord må være minst 6 tegn",
+    "oneNumber": "Passordet må inneholde minst ett tall",
+    "oneUppercase": "Passordet må inneholde minst en stor bokstav",
+    "oneLowercase": "Passordet må inneholde minst en liten bokstav",
+    "oneSpecialChar": "Passordet må inneholde minst ett spesialtegn",
+};
+
+const passwordConfirmationRequirements = {
+    "match": "Passordene må være like",
+};
+
+const nameRequirements = {
+    "minTwoChar": "Feltet må inneholde minst 2 tegn",
+};
+
+
+const communicateFirebaseError = (error: any) => {
+    switch (error.code) {
+        case 'auth/invalid-email':
+            return emailRequirements['correctFormat'];
+        case 'auth/weak-password':
+            return passwordRequirements['sixCharacters'];
+        case 'auth/email-already-in-use':
+            return signInRequirements['isInUse'];
+        case 'auth/wrong-password':
+            return signInRequirements['incorrectPassword'];
+        case 'auth/user-not-found':
+            return signInRequirements['noUser'];
+        default:
+            return error.message;
+    }
+}
+
 const FirebaseAuthUI = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [fornavn, setFornavn] = useState('');
-    const [etternavn, setEtternavn] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
     const [isRegisterMode, setIsRegisterMode] = useState(false);
     const [signedInUser, setSignedInUser] = useState<any>(null);
     const [isLoading, setIsLoading] = useState(true);
-    const [errors, setErrors] = useState<{ email?: string, password?: string, general?: string }>({});
+
+    const [firebaseErrors, setFirebaseErrors] = useState<string[]>([]);
+    const [emailErrors, setEmailErrors] = useState<string[]>([]);
+    const [passwordErrors, setPasswordErrors] = useState<string[]>([]);
+    const [passwordConfirmationErrors, setPasswordConfirmationErrors] = useState<string[]>([]);
+    const [nameErrors, setNameErrors] = useState<string[]>([]);
+    const [lastNameErrors, setLastNameErrors] = useState<string[]>([]);
 
     const handleGoogleSignIn = async () => {
         try {
             const result = await signInWithGoogle();
             if (result instanceof Error) {
-                setErrors({ general: result.message });
+                // add the error to the firebaseErrors
+                setFirebaseErrors([...firebaseErrors, result.message]);
                 return;
             }
             console.log('Sign in successful', result);
         } catch (error) {
             console.error('Google sign-in error:', error);
-            setErrors({ general: 'Google sign-in error' });
+            setFirebaseErrors([...firebaseErrors, 'Google sign-in error']);
         }
     };
+
+    const validateEmail = () => {
+
+        if (email === '') return setEmailErrors([]);
+
+        const errors = new Set<string>();
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!email.match(emailRegex)) {
+            errors.add(emailRequirements['correctFormat']);
+        } else {
+            errors.delete(emailRequirements['correctFormat']);
+        }
+
+        setEmailErrors(Array.from(errors));
+    };
+
+    const validatePassword = () => {
+
+        if (password === '') return setPasswordErrors([]);
+
+        const errors = new Set<string>();
+
+        if (password.length < 6) {
+            errors.add(passwordRequirements['sixCharacters']);
+        } else {
+            errors.delete(passwordRequirements['sixCharacters']);
+        }
+
+        if (!password.match(/[0-9]/)) {
+            errors.add(passwordRequirements['oneNumber']);
+        } else {
+            errors.delete(passwordRequirements['oneNumber']);
+        }
+
+        if (!password.match(/[A-Z]/)) {
+            errors.add(passwordRequirements['oneUppercase']);
+        } else {
+            errors.delete(passwordRequirements['oneUppercase']);
+        }
+
+        if (!password.match(/[a-z]/)) {
+            errors.add(passwordRequirements['oneLowercase']);
+        } else {
+            errors.delete(passwordRequirements['oneLowercase']);
+        }
+
+        if (!password.match(/[!@#$%^&*]/)) {
+            errors.add(passwordRequirements['oneSpecialChar']);
+        } else {
+            errors.delete(passwordRequirements['oneSpecialChar']);
+        }
+
+        validatePasswordConfirmation();
+        setPasswordErrors(Array.from(errors));
+
+    };
+
+    const validatePasswordConfirmation = () => {
+        const errors = new Set<string>();
+
+        if (password !== confirmPassword) {
+            errors.add(passwordConfirmationRequirements['match']);
+        } else {
+            errors.delete(passwordConfirmationRequirements['match']);
+        }
+
+        setPasswordConfirmationErrors(Array.from(errors));
+    }
+
+    const validateName = () => {
+        console.log("first name entered;", firstName)
+        if (firstName === '') return setNameErrors([]);
+        const errors = new Set<string>();
+
+        if (firstName.length < 2) {
+            errors.add(nameRequirements['minTwoChar']);
+        } else {
+            errors.delete(nameRequirements['minTwoChar']);
+        }
+
+        setNameErrors(Array.from(errors));
+    }
+
+    const validateLastName = () => {
+        console.log("last name entered;", lastName)
+        if (lastName === '') return setLastNameErrors([]);
+        const errors = new Set<string>();
+
+        if (lastName.length < 2) {
+            errors.add(nameRequirements['minTwoChar']);
+        } else {
+            errors.delete(nameRequirements['minTwoChar']);
+        }
+
+        setLastNameErrors(Array.from(errors));
+    }
+
+    useEffect(() => {
+        if (emailErrors.length > 0) validateEmail();
+    }, [email]);
+
+    useEffect(() => {
+        validatePassword();
+    }, [password]);
+
+    useEffect(() => {
+        validatePasswordConfirmation();
+    }, [confirmPassword]);
+
+    useEffect(() => {
+        if (nameErrors.length > 0) validateName();
+    }, [firstName]);
+
+    useEffect(() => {
+        if (lastNameErrors.length > 0) validateLastName();
+    }, [lastName]);
 
     const handleEmailSignIn = async () => {
         try {
             const result = await signInWithEmailAndPassword(email, password);
             if (result instanceof Error) {
                 console.log('Sign in error:', result.message);
-                if (result.message === 'auth/invalid-email - Firebase: Error (auth/invalid-email).' || result.message === 'auth/user-not-found') {
-                    setErrors({ email: 'Sjekk formatet på e-postadressen' });
-                } else if (result.message === 'auth/invalid-credential - Firebase: Error (auth/invalid-credential).') {
-                    setErrors({ general: 'E-postadressen eller passordet er feil' });
-                } else {
-                    setErrors({ general: result.message });
-                }
-
+                const errorMessage = communicateFirebaseError(result);
+                setFirebaseErrors([...firebaseErrors, errorMessage]);
                 return;
             }
             console.log('Sign in successful', result);
         } catch (error) {
             console.error('Email sign-in error:', error);
-            setErrors({ general: 'Email sign-in error' });
+            setFirebaseErrors([...firebaseErrors, 'Email sign-in error']);
         }
     };
 
     const handleRegister = async () => {
         try {
-            const result = await registerUser(email, password, fornavn, etternavn);
+            const result = await registerUser(email, password, firstName, lastName);
 
             if (result instanceof Error) {
                 console.log('Register error:', result.message);
-                if (result.message === 'auth/invalid-email - Firebase: Error (auth/invalid-email).') {
-                    setErrors({ email: 'Sjekk formatet på e-postadressen' });
-                } 
-                if (result.message === 'auth/weak-password') {
-                    setErrors({ password: 'Passordet er for svakt' });
-                }
-                if (result.message === 'auth/email-already-in-use - Firebase: Error (auth/email-already-in-use).') {
-                    setErrors({ email: 'E-postadressen er allerede i bruk' });
-                }
+                const errorMessage = communicateFirebaseError(result);
+                setFirebaseErrors([...firebaseErrors, errorMessage]);
                 return;
             }
             console.log('Register successful', result);
         } catch (error: any) {
             console.log(error.message);
-            setErrors({ general: error.message });
+            setFirebaseErrors([...firebaseErrors, error.message]);
         }
     };
 
@@ -102,7 +259,17 @@ const FirebaseAuthUI = () => {
         return unsubscribe;
     }, []);
 
-    const isRegisterEnabled = email && password && fornavn && etternavn;
+    const isRegisterEnabled =
+        emailErrors.length === 0 &&
+        passwordErrors.length === 0 &&
+        passwordConfirmationErrors.length === 0 &&
+        nameErrors.length === 0 &&
+        lastNameErrors.length === 0 &&
+        email !== '' &&
+        password !== '' &&
+        confirmPassword !== '' &&
+        firstName !== '' &&
+        lastName !== '';
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -145,9 +312,9 @@ const FirebaseAuthUI = () => {
                 variant="outlined"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                error={!!errors.email}
-                helperText={errors.email}
-                
+                onBlur={validateEmail}
+                error={emailErrors.length > 0}
+                helperText={emailErrors}
             />
             <TextField
                 required
@@ -158,9 +325,9 @@ const FirebaseAuthUI = () => {
                 fullWidth
                 variant="outlined"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                error={!!errors.password}
-                helperText={errors.password}
+                onChange={(e: { target: { value: SetStateAction<string>; }; }) => [setPassword(e.target.value)]}
+                error={isRegisterMode && passwordErrors.length > 0}
+                helperText={isRegisterMode && passwordErrors}
             />
 
             {isRegisterMode && (
@@ -175,8 +342,8 @@ const FirebaseAuthUI = () => {
                         variant="outlined"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
-                        error={!!errors.password}
-                        helperText={errors.password}
+                        error={passwordConfirmationErrors.length > 0}
+                        helperText={passwordConfirmationErrors}
                     />
                     <TextField
                         required
@@ -186,8 +353,11 @@ const FirebaseAuthUI = () => {
                         type="text"
                         fullWidth
                         variant="outlined"
-                        value={fornavn}
-                        onChange={(e) => setFornavn(e.target.value)}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        error={nameErrors.length > 0}
+                        helperText={nameErrors}
+                        onBlur={validateName}
 
                     />
                     <TextField
@@ -198,15 +368,18 @@ const FirebaseAuthUI = () => {
                         type="text"
                         fullWidth
                         variant="outlined"
-                        value={etternavn}
-                        onChange={(e) => setEtternavn(e.target.value)}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        error={lastNameErrors.length > 0}
+                        helperText={lastNameErrors}
+                        onBlur={validateLastName}
                     />
                 </>
             )}
 
-            {errors.general && (
+            {firebaseErrors.length > 0 && (
                 <Typography color="error" variant="body2" style={{ marginTop: '10px' }}>
-                    {errors.general}
+                    {firebaseErrors.join(', ')}
                 </Typography>
             )}
 
@@ -223,14 +396,14 @@ const FirebaseAuthUI = () => {
                 {isRegisterMode ? (
                     <p>
                         Allerede en bruker?{' '}
-                        <StyledLinkSpan onClick={() => { setIsRegisterMode(false); setErrors({}); }}>
+                        <StyledLinkSpan onClick={() => { setIsRegisterMode(false); }}>
                             Logg inn
                         </StyledLinkSpan>
                     </p>
                 ) : (
                     <p>
                         Ny bruker?{' '}
-                        <StyledLinkSpan onClick={() => { setIsRegisterMode(true); setErrors({}); }}>
+                        <StyledLinkSpan onClick={() => { setIsRegisterMode(true); }}>
                             Registrer
                         </StyledLinkSpan>
                     </p>
